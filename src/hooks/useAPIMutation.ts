e3f5086c91f5d55fc1a9e8f609fb4e8bbc5cd1dc
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-07-07 12:13:18 +0500

    added params

--- src/hooks/useAPIMutation.ts
+++ src/hooks/useAPIMutation.ts
@@ -6,15 +6,17 @@ type Args<Response, Error, Variables> = {
   url: string;
   method?: "POST" | "DELETE" | "PUT" | "PATCH";
   options?: UseMutationOptions<AxiosResponse<Response>, Error, Variables>;
+  params?: object;
 };
 
 const useAPIMutation = <Variables = any, Response = any, Error = any>({
   method = "POST",
   options = {},
   url,
+  params,
 }: Args<Response, Error, Variables>) =>
   useMutation((variables) => {
-    const response = request({ method: method, url, data: variables });
+    const response = request({ method: method, url, params, data: variables });
     return response;
   }, options);
 
