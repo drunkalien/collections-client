2637fb58f2ef640ce30b6259cbcf4b2e38efed01
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-07-07 12:22:40 +0500

    added get static paths

--- src/pages/collections/[collection]/items/create.tsx
+++ src/pages/collections/[collection]/items/create.tsx
@@ -1,5 +1,5 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { GetStaticProps, NextPage } from "next";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 
 import { CreateItem } from "components";
 
@@ -8,6 +8,15 @@ const Create: NextPage = () => {
 };
 
 export default Create;
+
+export const getStaticPaths: GetStaticPaths<{
+  collection: string;
+}> = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   const localeProps = await serverSideTranslations(locale || "", ["common"]);
