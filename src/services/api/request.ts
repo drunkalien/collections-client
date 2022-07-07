e62ac3ca97eacefc8d9c4b7592326518f671cf46
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-07-07 12:14:03 +0500

    added type

--- src/services/api/request.ts
+++ src/services/api/request.ts
@@ -1,19 +1,19 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, AxiosRequestConfig } from "axios";
 
 export const requestInstance = axios.create({
   baseURL: "http://localhost:5000/api/v1/",
 });
 
-requestInstance.interceptors.request.use((config: any) => {
+requestInstance.interceptors.request.use((config: AxiosRequestConfig) => {
   const token = window.localStorage.getItem("token");
 
-  config.headers["Authorization"] = `Bearer ${token}`;
+  config["headers"]!["Authorization"]! = `Bearer ${token}`;
 
   return config;
 });
 
 requestInstance.interceptors.response.use(
-  (config: any) => config,
+  (config: AxiosRequestConfig) => config,
   (error: AxiosError) => {
     if (error.response?.status === 401) {
       window.location.replace("/login");
