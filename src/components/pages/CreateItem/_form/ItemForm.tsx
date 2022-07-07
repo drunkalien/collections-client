1acfc11269b1df6de5453d30c81a1c3874efd026
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-07-07 12:12:50 +0500

    added mutation and some changes

--- src/components/pages/CreateItem/_form/ItemForm.tsx
+++ src/components/pages/CreateItem/_form/ItemForm.tsx
@@ -2,10 +2,13 @@ import { useForm, useFieldArray } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import cn from "classnames";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useTranslation } from "next-i18next";
 
 import { CustomFieldsType, SchemaValues } from "types";
-import { Input, Select, Button } from "components";
-import { useTranslation } from "next-i18next";
+import { Input, Select, Button, Textarea } from "components";
+import { useAPIMutation, useAPIQuery, useCurrentUser } from "hooks";
 
 type FormValues = {
   name: string;
@@ -19,39 +22,39 @@ const schema = yup.object<SchemaValues<FormValues>>({
   // customFields: yup.array(),
 });
 
-const dummyFields: CustomFieldsType[] = [
-  {
-    label: "Author",
-    type: "text",
-    value: "",
-  },
-  {
-    label: "Synopsis",
-    type: "textbox",
-    value: "",
-  },
-  {
-    label: "Published at",
-    type: "date",
-    value: "",
-  },
-];
-
 const ItemForm = () => {
   const { t } = useTranslation();
   const { register, control, handleSubmit } = useForm<FormValues>({
     resolver: yupResolver(schema),
   });
-  const { fields } = useFieldArray({
+
+  const userQuery = useCurrentUser();
+
+  const router = useRouter();
+  const { collection } = router.query;
+  const itemMutation = useAPIMutation({
+    url: "items",
+    params: { collectionId: collection, userId: userQuery.data?.user._id },
+  });
+  const fieldsQuery = useAPIQuery<{
+    customFields: CustomFieldsType[];
+    _id: string;
+    parent: string;
+  }>({
+    url: `custom-fields/${collection}`,
+  });
+  const customFields = fieldsQuery.data?.customFields || [];
+
+  useFieldArray({
     control,
     name: "customFields",
   });
 
-  const submit = (data: FormValues) => {
-    for (let i = 0; i < dummyFields.length; i++) {
-      dummyFields[i] = { ...dummyFields[i], ...data.customFields[i] };
+  const submit = async (data: FormValues) => {
+    for (let i = 0; i < customFields.length; i++) {
+      data.customFields[i] = { ...customFields[i], ...data.customFields[i] };
     }
-    console.log(dummyFields);
+    await itemMutation.mutateAsync(data);
   };
 
   return (
@@ -60,20 +63,28 @@ const ItemForm = () => {
       onSubmit={handleSubmit(submit)}
     >
       <div className="mt-4">
-        <Input label={t("Name")} />
+        <Input label={t("Name")} {...register("name")} />
       </div>
       <div className="mt-4">
-        <Input label={t("Tags")} />
+        <Input label={t("Tags")} {...register("tags")} />
       </div>
-      {dummyFields.map((field, idx) => (
-        <div className="mt-4" key={idx}>
-          <Input
-            type={field.type}
-            label={field.label}
-            {...register(`customFields.${idx}.value`)}
-          />
-        </div>
-      ))}
+      {customFields.length > 0 &&
+        customFields?.map((field, idx) => (
+          <div className="mt-4" key={idx}>
+            {field.type !== "textarea" ? (
+              <Input
+                type={field.type}
+                label={field.label}
+                {...register(`customFields.${idx}.value`)}
+              />
+            ) : (
+              <Textarea
+                label={field.label}
+                {...register(`customFields.${idx}.value`)}
+              />
+            )}
+          </div>
+        ))}
       <Button type="submit">Create Item</Button>
     </form>
   );
