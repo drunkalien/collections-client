5d8aee550d70627143e4ba16833d27ac9223ee55
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-07-10 14:40:43 +0500

    exported UserData type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,7 @@
 export { type FieldType } from "./fieldType";
 export { type CustomFieldsType } from "./customFields";
 export { type SchemaValues } from "./schemaValues";
-export { type User } from "./UserType";
+export { type User, type UserData } from "./UserType";
 export { type ItemType } from "./item";
 export { type Comment } from "./comment";
 export { type CommentData } from "./comment";
