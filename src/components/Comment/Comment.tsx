0cabc85dc8c8032f0b0c62f2118bc3db7d07cf76
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-07-10 14:39:06 +0500

    fix

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,14 +1,14 @@
 import dayjs from "dayjs";
 
 import { useAPIQuery } from "hooks";
-import { CommentData, User } from "types";
+import { CommentData, UserData } from "types";
 
 type Props = {
   comment: CommentData;
 };
 
 const Comment = ({ comment }: Props) => {
-  const authorQuery = useAPIQuery<User>({
+  const authorQuery = useAPIQuery<UserData>({
     url: `users/user-by-id/${comment.author}`,
   });
 
@@ -17,18 +17,18 @@ const Comment = ({ comment }: Props) => {
       <div>
         <img
           className="w-[70px] h-[70px] rounded-full bg-gray overflow-hidden"
-          src={authorQuery.data?.user?.avatar}
-          alt={authorQuery.data?.user?.username}
+          src={authorQuery.data?.avatar}
+          alt={authorQuery.data?.username}
           width={70}
           height={70}
         />
       </div>
       <div>
-        <div className="font-bold">{authorQuery.data?.user?.username}</div>
+        <div className="font-bold">{authorQuery.data?.username}</div>
         <div className="text-gray">
-          {dayjs(authorQuery.data?.user?.createdAt).format("DD.MM.YYYY")}
+          {dayjs(authorQuery.data?.createdAt).format("DD.MM.YYYY")}
         </div>
-        <div> {comment.body}</div>
+        <div>{comment.body}</div>
       </div>
     </div>
   );
