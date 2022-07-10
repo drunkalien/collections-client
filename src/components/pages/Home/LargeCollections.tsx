b7c8a4310b515fb81e31aab6b098711fb2362778
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-07-10 10:38:19 +0500

    some changes

--- src/components/pages/Home/LargeCollections.tsx
+++ src/components/pages/Home/LargeCollections.tsx
@@ -4,27 +4,32 @@ import Paper from "components/Paper";
 const dummyCollections = [
   {
     name: "Collection1",
+    id: 213127,
     author: "Author 1",
   },
   {
     name: "Collection2",
     author: "Author 2",
+    id: 95302,
     image:
       "https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&fit=clip",
   },
   {
     name: "Collection3",
     author: "Author 3",
+    id: 3242342,
     image:
       "https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&fit=clip",
   },
   {
     name: "Collection4",
+    id: 643534,
     author: "Author 4",
   },
   {
     name: "Collection5",
     author: "Author 5",
+    id: 123123,
     image:
       "https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&fit=clip",
   },
@@ -33,8 +38,14 @@ const dummyCollections = [
 const LargeCollections = () => {
   return (
     <div>
-      {dummyCollections.map(({ author, image, name }, idx) => (
-        <Collection author={author} name={name} image={image} key={idx} />
+      {dummyCollections.map(({ author, image, name, id }, idx) => (
+        <Collection
+          id={id}
+          author={author}
+          name={name}
+          image={image}
+          key={idx}
+        />
       ))}
     </div>
   );
